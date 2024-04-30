7d679963b20d58af340a658acdae965a7d9e3f7c
Silich Alexander <os@sun.agency>
2024-04-30 16:30:41 +0300

    add script

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,61 @@
 import "modern-normalize";
 import "/js/swiper";
+import refs from "./refs";
+
+const isHiddenClassName = "is-hidden";
+
+refs.phoneBtn.addEventListener("click", onPhoneBtnClick);
+refs.consultationBtn.addEventListener("click", toggleModalWinState);
+refs.heroJoinUsBtn.addEventListener("click", toggleModalWinState);
+refs.closeModalWinBtn.addEventListener("click", toggleModalWinState);
+refs.vacanciesList.addEventListener("click", toggleShowMoreVacancyDetails);
+refs.vacanciesList.addEventListener("click", onJoinUsBtnClick);
+refs.questionsList.addEventListener("click", toggleShowAnswer);
+
+function onPhoneBtnClick(e) {
+  e.currentTarget.blur();
+  refs.phoneBtnLink.classList.toggle(isHiddenClassName);
+}
+
+function toggleModalWinState(e) {
+  e.currentTarget.blur();
+  refs.joinUsModalWin.classList.toggle(isHiddenClassName);
+}
+
+function toggleShowMoreVacancyDetails(e) {
+  const isTargetToggleShowMoreBtn =
+    e.target.classList.contains("js-toggle-show-more-vacancy-details") ||
+    e.target.closest(".js-toggle-show-more-vacancy-details");
+
+  if (!isTargetToggleShowMoreBtn) {
+    return;
+  }
+
+  const vacancyDetails = e.target
+    .closest(".js-vacancy-card")
+    .querySelector(".js-vacancy-card-details");
+  vacancyDetails.classList.toggle(isHiddenClassName);
+}
+
+function onJoinUsBtnClick(e) {
+  e.currentTarget.blur();
+  const isTargetJoinUsBtn = e.target.closest(".js-join-us-btn");
+
+  if (!isTargetJoinUsBtn) {
+    return;
+  }
+
+  refs.joinUsModalWin.classList.toggle(isHiddenClassName);
+}
+
+function toggleShowAnswer(e) {
+  const targetClassName = "hidden-desc";
+  const isTargetQuestionBtn = e.target.closest(".js-question-btn");
+
+  if (!isTargetQuestionBtn) {
+    return;
+  }
+
+  const questionContainer = e.target.closest(".js-question-wrap");
+  questionContainer.classList.toggle(targetClassName);
+}
---
+++ src/js/refs.js
@@ -0,0 +1,12 @@
+const refs = {
+  phoneBtn: document.querySelector(".js-phone-btn"),
+  phoneBtnLink: document.querySelector(".js-phone-btn-link"),
+  consultationBtn: document.querySelector(".js-consultation-btn"),
+  joinUsModalWin: document.querySelector(".js-join-us-modal-win"),
+  closeModalWinBtn: document.querySelector(".js-close-modal-win-btn"),
+  heroJoinUsBtn: document.querySelector(".js-hero-join-us-btn"),
+  vacanciesList: document.querySelector(".js-vacancies-list"),
+  questionsList: document.querySelector(".js-questions-list"),
+};
+
+export default refs;
