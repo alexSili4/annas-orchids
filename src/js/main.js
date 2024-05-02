45c5afc4f69a413e23937b17654db72e54f348cd
Silich Alexander <os@sun.agency>
2024-05-02 09:45:09 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import '/js/swiper';
+import '/js/aos';
 import '/js/eventHandlers';
 import '/js/validateJoinUsForm';
-import '/js/aos';
 import '/js/setWyWeCounter';
 import '/js/scrollToTargetSection';
