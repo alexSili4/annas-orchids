76bcf7ba6f7c9b7278ce93353ae9b6d5d2337da2
Silich Alexander <os@sun.agency>
2024-05-02 09:42:54 +0300

    Update swiper.js

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,22 +1,38 @@
-import Swiper from "swiper";
-import { Navigation, Pagination } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
-const swiper = new Swiper(".swiper", {
+const swiperClassName = '.swiper';
+
+const swiper = new Swiper(swiperClassName, {
   modules: [Navigation, Pagination],
-  direction: "horizontal",
+  direction: 'horizontal',
   spaceBetween: 50,
   speed: 800,
   loop: true,
   grabCursor: true,
   navigation: {
-    nextEl: ".slider-button-next",
-    prevEl: ".slider-button-prev",
+    nextEl: '.slider-button-next',
+    prevEl: '.slider-button-prev',
   },
   pagination: {
-    clickable:true,
-    el: ".slider-pagination",
+    clickable: true,
+    el: '.slider-pagination',
+  },
+  on: {
+    transitionEnd: function () {
+      const sliders = document.querySelectorAll(swiperClassName);
+      sliders.forEach((el) => {
+        const sliderDesc = el.querySelectorAll('.js-hidden-desc');
+        sliderDesc.forEach((el) => {
+          const isHiddenDesc = el.classList.contains('hidden-desc');
+          if (!isHiddenDesc) {
+            el.classList.toggle('hidden-desc');
+          }
+        });
+      });
+    },
   },
 });
